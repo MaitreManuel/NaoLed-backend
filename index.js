b66a9b6abe1276f1d436f23629a3a34e80cc00e8
Herve <contact@herve-hochart.fr>
2018-11-28 00:59:33 +0100

    :whale: (Global) Docker conf ! docker-compose up

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const bodyParser = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-require('./route/ashbin')(app);
-require('./route/trash')(app);
+require('./route/ashbin')(app)
+require('./route/trash')(app)
 
-const PORT = process.env.PORT || 5000;
-const DB_LINK = 'mongodb://127.0.0.1:27017/naoled';
-const DB_OPTIONS = { useNewUrlParser: true };
+const PORT = process.env.PORT || 5000
+const DB_LINK = 'mongodb://mongo:27017/naoled'
+const DB_OPTIONS = { useNewUrlParser: true }
 
-mongoose.connect(DB_LINK, DB_OPTIONS);
+mongoose
+  .connect(
+    DB_LINK,
+    DB_OPTIONS
+  )
+  .catch((e) => console.log(e))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 server.listen(PORT, () => {
-  console.log(`Server servin' from good ol' port ${PORT}`);
-});
+  console.log(`Server servin' from good ol' port ${PORT}`)
+})
 
 app.get('/', (req, res) => {
-  console.log('Route /');
-  res.send('Route /');
-});
+  console.log('Route /')
+  res.send('Route /')
+})
