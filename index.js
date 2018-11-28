82c79c1bb769b9d0ee0efbde9e0e5e4b9561441a
Trystan Eveno <trystan.eveno@lephare.com>
2018-11-28 09:41:13 +0100

    config .env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -9,8 +10,8 @@ const io = require('socket.io')(server);
 require('./route/ashbin')(app);
 require('./route/trash')(app);
 
-const PORT = process.env.PORT || 5000;
-const DB_LINK = 'mongodb://127.0.0.1:27017/naoled';
+const PORT = process.env.DB_PORT;
+const DB_LINK = process.env.DB_HOST;
 const DB_OPTIONS = { useNewUrlParser: true };
 
 mongoose.connect(DB_LINK, DB_OPTIONS);
