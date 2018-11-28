b8e24b4d66dc057f65e8f6683b188dbe49382d51
Herve <contact@herve-hochart.fr>
2018-11-28 01:02:40 +0100

    :whale: (global) oops, revert index.js semicolumns

--- index.js
+++ index.js
@@ -4,40 +4,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-require('./route/ashbin')(app)
-require('./route/trash')(app)
+require('./route/ashbin')(app);
+require('./route/trash')(app);
 
 const PORT = process.env.DB_PORT;
 const DB_LINK = process.env.DB_HOST;
 const DB_OPTIONS = { useNewUrlParser: true };
 
-mongoose
-  .connect(
-    DB_LINK,
-    DB_OPTIONS
-  )
-  .catch((e) => console.log(e))
+mongoose.connect(DB_LINK, DB_OPTIONS);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 server.listen(PORT, () => {
-  console.log(`Server servin' from good ol' port ${PORT}`)
-})
+  console.log(`Server servin' from good ol' port ${PORT}`);
+});
 
 app.get('/', (req, res) => {
-  console.log('Route /')
-  res.send('Route /')
-})
+  console.log('Route /');
+  res.send('Route /');
+});
