69b56984e346fdb473aeed184736ea619770fbd8
Herve <contact@herve-hochart.fr>
2018-11-30 17:01:42 +0100

    :bug: (oops forgot coments)

--- routes/lightRoutes.js
+++ routes/lightRoutes.js
@@ -8,45 +8,25 @@ const Light = require('../models/light');
 
 module.exports = app => {
   LightRouter.route('/')
-    // .get((req, res) => {
-    //   helpers.getAll(Light, ({ error, result }) => {
-    //     if (error) {
-    //       res.send(error);
-    //     } else {
-    //       result.length < 1 ? res.status(418).send({ 'message': 'Aucun résultat' }) : res.send(result);
-    //     }
-    //   });
-    // })
-
-    // .post(({body: { name, status }}, res) => {
-    //   arduinoHelpers.getByName(name, ({ error, result }) => {
-    //     if (error) {
-    //       res.send(error);
-    //     } else {
-    //       if (result.length < 1) {
-    //         res.status(418).send({ 'message': 'Identifiant faux' });
-    //       } else {
-    //         lightHelpers.setLight(status, ({ error, result }) => {
-    //           if (error) {
-    //             res.send(error);
-    //           } else {
-    //             res.send(result);
-    //           }
-    //         });
-    //       }
-    //     }
-    //   })
-    // })
-
     .get((req, res) => {
-      arduinoHelpers.getByName(req.query.name, ({ error, result }) => {
+      helpers.getAll(Light, ({ error, result }) => {
+        if (error) {
+          res.send(error);
+        } else {
+          result.length < 1 ? res.status(418).send({ 'message': 'Aucun résultat' }) : res.send(result);
+        }
+      });
+    })
+
+    .post(({body: { name, status }}, res) => {
+      arduinoHelpers.getByName(name, ({ error, result }) => {
         if (error) {
           res.send(error);
         } else {
           if (result.length < 1) {
             res.status(418).send({ 'message': 'Identifiant faux' });
           } else {
-            lightHelpers.setLight(req.query.status, ({ error, result }) => {
+            lightHelpers.setLight(status, ({ error, result }) => {
               if (error) {
                 res.send(error);
               } else {
